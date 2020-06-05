d059fb91fe681f3dd22907e329cc61cb3cb51258
vieiraigor8787 <igor_vc@hotmail.com>
2020-06-05 20:38:02 -0300

    first steps mobile

--- App.tsx
+++ App.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { StatusBar } from 'react-native';
+
+import { Roboto_500Medium, Roboto_400Regular } from '@expo-google-fonts/roboto';
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+
+import Home from './src/pages/Home';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_500Medium,
+    Roboto_400Regular,
+    Ubuntu_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <>
+    <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+    <Home />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
