6632d77e5d0a38ffe287dd4f3013bdd0546f432c
vieiraigor8787 <igor_vc@hotmail.com>
2020-06-06 20:25:49 -0300

    seguinda vida no mobile

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -1,88 +1,109 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Constants from 'expo-constants';
 import { Feather as Icon } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image } from 'react-native';
 import  MapView, { Marker } from 'react-native-maps';
 import { SvgUri } from 'react-native-svg';
+import api from '../../services/api';
+
+interface Item {
+  id: number;
+  title: string;
+  icone_url: string;
+}
 
 const Points = () => {
-    const navigation = useNavigation();
-    
-    function handleNavigateBack() {
-        navigation.goBack();
+  const [items, setItems] = useState<Item[]>([]);
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    api.get('items').then(response => {
+      setItems(response.data)
+    });
+  }, []);
+  
+  function handleNavigateBack() {
+      navigation.goBack();
+  }
+
+  function handleNavigateToDetail() {
+      navigation.navigate('Detail');
+  }
+
+  function handleSelectItem(id: number) {
+    const alreadySelected = selectedItems.findIndex(item => item === id);
+
+    if (alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter(item => item !== id);
+
+        setSelectedItems(filteredItems);
+    } else{ 
+        setSelectedItems([ ...selectedItems, id ]);
     }
+  }
 
-    function handleNavigateToDetail() {
-        navigation.navigate('Detail');
-    }
+  return (
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
 
-    return (
-      <>
-        <View style={styles.container}>
-          <TouchableOpacity onPress={handleNavigateBack}>
-            <Icon name="arrow-left" size={20} color="#34cb79" />
-          </TouchableOpacity>
+        <Text style={styles.title}>Bem vindo.</Text>
+        <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
 
-          <Text style={styles.title}>Bem vindo.</Text>
-          <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
-
-          <View style={styles.mapContainer} >
-            <MapView 
-              style={styles.map} 
-              initialRegion={{ 
-                latitude: -23.5429771, 
-                longitude: -46.757523,
-                latitudeDelta: 0.014,
-                longitudeDelta: 0.014,
+        <View style={styles.mapContainer} >
+          <MapView 
+            style={styles.map} 
+            initialRegion={{ 
+              latitude: -23.5429771, 
+              longitude: -46.757523,
+              latitudeDelta: 0.014,
+              longitudeDelta: 0.014,
+            }}
+          >
+            <Marker 
+              style={styles.mapMarker} 
+              coordinate={{ 
+                  latitude: -23.5429771, 
+                  longitude: -46.757523 
               }}
+              onPress={handleNavigateToDetail}
             >
-              <Marker 
-                style={styles.mapMarker} 
-                coordinate={{ 
-                    latitude: -23.5429771, 
-                    longitude: -46.757523 
-                }}
-                onPress={handleNavigateToDetail}
+              <View style={styles.mapMarkerContainer}>
+                <Image style={styles.mapMarkerImage} source={{ uri: "https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" }} />
+                <Text style={styles.mapMarkerTitle}>Mercado</Text>
+              </View>
+            </Marker>
+          </MapView>
+        </View>
+      </View>
+      <View style={styles.itemsContainer}>
+        <ScrollView 
+          horizontal 
+          showsVerticalScrollIndicator={false} 
+          contentContainerStyle={{ paddingHorizontal: 20 }}
+          >
+           {items.map(item => (
+             <TouchableOpacity 
+               key={String(item.id)}
+               style={[
+                 styles.item,
+                 selectedItems.includes(item.id) ? styles.selectedItem : {}
+               ]} 
+               activeOpacity={.6}
+               onPress={() => handleSelectItem(item.id)}
               >
-                <View style={styles.mapMarkerContainer}>
-                  <Image style={styles.mapMarkerImage} source={{ uri: "https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" }} />
-                  <Text style={styles.mapMarkerTitle}>Mercado</Text>
-                </View>
-              </Marker>
-            </MapView>
-          </View>
-        </View>
-        <View style={styles.itemsContainer}>
-          <ScrollView 
-            horizontal 
-            showsVerticalScrollIndicator={false} 
-            contentContainerStyle={{ paddingHorizontal: 20 }}
-           >
-            <TouchableOpacity style={styles.item}>
-                <SvgUri width={42} height={42} uri="http://192.168.15.16:3333/uploads/baterias.svg"/>
-                <Text style={styles.itemTitle}>Lâmpadas</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.item}>
-                <SvgUri width={42} height={42} uri="http://192.168.15.16:3333/uploads/baterias.svg"/>
-                <Text style={styles.itemTitle}>Lâmpadas</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.item}>
-                <SvgUri width={42} height={42} uri="http://192.168.15.16:3333/uploads/baterias.svg"/>
-                <Text style={styles.itemTitle}>Lâmpadas</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.item}>
-                <SvgUri width={42} height={42} uri="http://192.168.15.16:3333/uploads/baterias.svg"/>
-                <Text style={styles.itemTitle}>Lâmpadas</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.item}>
-                <SvgUri width={42} height={42} uri="http://192.168.15.16:3333/uploads/baterias.svg"/>
-                <Text style={styles.itemTitle}>Lâmpadas</Text>
-            </TouchableOpacity>
-          </ScrollView>
-        </View>
-      </>
-    );
+                <SvgUri width={42} height={42} uri={item.icone_url}/>
+                <Text style={styles.itemTitle}>{item.title}</Text>
+             </TouchableOpacity>
+           ))}
+        </ScrollView>
+      </View>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
